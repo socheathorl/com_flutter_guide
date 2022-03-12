b03741dbd7f18e5177f46d87b12b96e23484fbde
FlutLab.io <>
2022-03-12 14:49:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text.'),
+      ),
+    );
   }
 }
