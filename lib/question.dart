6f04f4486eb23f239d0a761b96a27611a7d866f1
FlutLab.io <>
2022-03-13 08:52:40 +0000

    Autosave

---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text(questionText);
+  }
+}
