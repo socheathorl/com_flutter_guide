1459dd5445ac671d9409a280341dca4f756ae30c
FlutLab.io <>
2022-03-13 08:04:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favourite color?",
+      "What's your favourit animal?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text with commit.'),
+        body: Column(
+          children: [
+            Text('The question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('Answer 3 chosen!'),
+              }
+            )
+          ],
+        ),
       ),
     );
   }
