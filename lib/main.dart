0769ef5316dae6c3daa7ee166f7c2762aa202b29
FlutLab.io <>
2022-03-13 10:47:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -45,9 +45,6 @@ class _MyAppState extends State<MyApp> {
   ];
   int _questionIndex = 0;
   void _answerQuestion() {
-    if (_questionIndex >= questions.length) {
-      _questionIndex = 0;
-    }
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -61,14 +58,18 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questionText'] as String),
-            ...(questions[_questionIndex]['answer'] as List<String>).map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['questionText'] as String),
+                  ...(questions[_questionIndex]['answer'] as List<String>).map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList(),
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
