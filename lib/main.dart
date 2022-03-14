ec9f8aaf1bccbddc930df661895e855dd73b2c81
FlutLab.io <>
2022-03-14 15:03:13 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,73 @@
 import 'package:flutter/material.dart';
-import './quiz.dart';
-import './result.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': "What's your favourite color?",
-      'answer': [
-        {
-          'text': 'Red',
-          'score': 10
-        },
-        {
-          'text': 'Blue',
-          'score': 8
-        },
-        {
-          'text': 'White',
-          'score': 5
-        },
-        {
-          'text': 'Green',
-          'score': 2
-        },
-      ]
-    },
-    {
-      'questionText': "What's your favourite animal?",
-      'answer': [
-        {
-          'text': 'Dog',
-          'score': 9
-        },
-        {
-          'text': 'Cat',
-          'score': 5
-        },
-        {
-          'text': 'Rabbit',
-          'score': 10
-        },
-        {
-          'text': 'Cow',
-          'score': 3
-        },
-      ]
-    },
-    {
-      'questionText': "What's your favourite phone?",
-      'answer': [
-        {
-          'text': 'IPhone',
-          'score': 9
-        },
-        {
-          'text': 'Samsung',
-          'score': 10
-        },
-        {
-          'text': 'Mi',
-          'score': 8
-        },
-        {
-          'text': 'Vivo',
-          'score': 6
-        },
-      ]
-    }
-  ];
-  int _questionIndex = 0;
-  int _totalScore = 0;
-  void _answerQuestion(int score) {
-    _totalScore += score;
-
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print('Answer chosen!');
-  }
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                questions: _questions,
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-              )
-            : Result(_totalScore, _resetQuiz),
+      title: 'Expense Planner',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Planner'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(tx.amount.toString()),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
