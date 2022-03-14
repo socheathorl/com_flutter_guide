6ce3d37cc6e22f2316d068c7c10f3113d5ff4bfd
FlutLab.io <>
2022-03-14 15:08:07 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,13 @@ class MyHomePage extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2),
+                      ),
                       child: Text(tx.amount.toString()),
                     ),
                     Column(
